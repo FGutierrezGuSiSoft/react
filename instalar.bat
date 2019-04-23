c9da14f44fe929519ea515ab5d61b423fdcbbb2f
FGutierrezGuSiSoft <48395586+FGutierrezGuSiSoft@users.noreply.github.com>
2019-04-22 23:05:31 -0400

    Update instalar.bat

--- instalar.bat
+++ instalar.bat
@@ -1,1 +1,1 @@
-docker run -it -p 3000:3000 -v /Users/groupon/Documents/Autonoma/react/app:/usr/src/app debian:stretch bash
+docker run -it -p 3000:3000 -v c:/users/alumnoing/downloads/react-install/app:/usr/src/app debian:stretch bash
